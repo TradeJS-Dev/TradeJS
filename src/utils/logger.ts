76cc846d352b61b812c27a4b0bb2638ec16d487b
aleksnick <nikolaev@iprojects.ru>
2025-05-20 22:16:44 +0300

    feat: update logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,14 +1,15 @@
 import { createLogger, transports, format } from 'winston';
 
 export const logger = createLogger({
-  format: format.combine(
-    format.timestamp(),
-    format.colorize({ all: true }),
-    format.splat(),
-    format.simple(),
-  ),
+  format: format.combine(format.timestamp(), format.splat(), format.simple()),
   transports: [
-    new transports.Console(),
+    // Логируем всё в консоль (включая цвет)
+    new transports.Console({
+      format: format.combine(format.colorize({ all: true }), format.simple()),
+    }),
+
     new transports.File({ filename: 'service.log' }),
+
+    new transports.File({ filename: 'error.log', level: 'error' }),
   ],
 });
