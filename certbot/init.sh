62ac34218a02aafaa2fd80cdd08eb21146b2fc62
aleksnick <nikolaev@iprojects.ru>
2025-10-08 21:21:31 +0300

    fix: redis pwd env

--- certbot/init.sh
+++ certbot/init.sh
@@ -2,5 +2,6 @@
 if [ ! -d "/etc/letsencrypt/live/aleksnick01inv.fvds.ru" ]; then
   certbot certonly --webroot -w /var/www/certbot \
     --email aleksnick01@gmail.com --agree-tos --no-eff-email \
-    -d aleksnick01inv.fvds.ru
+    -d aleksnick01inv.fvds.ru \
+    -d redisinsight.aleksnick01inv.fvds.ru
 fi
