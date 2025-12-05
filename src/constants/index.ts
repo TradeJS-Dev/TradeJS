2965a1663a2dc29d78c57dda66199469e6aaa7a2
aleksnick <nikolaev@iprojects.ru>
2025-12-05 23:31:30 +0300

    fix: constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,10 +5,10 @@ const { NODE_ENV } = process.env;
 
 // Минимальное допустимое движение между точками линии
 export const TP_MIN_PERCENT = 2;
-export const TP_MAX_LONG_PERCENT = 4.5;
-export const TP_MAX_SHORT_PERCENT = 3.5;
+export const TP_MAX_LONG_PERCENT = 3.5;
+export const TP_MAX_SHORT_PERCENT = 2.5;
 export const TP_DISTANCE = 1 / 3;
-export const SL_PERCENT = 0.8;
+export const SL_PERCENT = 0.7;
 export const MAX_LOSS_VALUE = 0.5;
 export const MIN_RISK_RATIO = 1.2;
 
