228d74aaf2af535331369ae5334f6649e9c2752c
aleksnick <nikolaev@iprojects.ru>
2026-05-20 21:49:34 +0300

    Disable createMoveStopToBreakEvenOnBarHook

--- tradejs.config.ts
+++ tradejs.config.ts
@@ -1,12 +1,4 @@
 import { defineConfig } from '@tradejs/core/config';
 import { basePreset } from '@tradejs/base';
-import { createMoveStopToBreakEvenOnBarHook } from '@tradejs/node/strategies';
 
-export default defineConfig(basePreset, {
-  hooks: {
-    onBar: createMoveStopToBreakEvenOnBarHook({
-      triggerRiskMultiplier: 0.8,
-      stopProfitMultiplier: 0.2,
-    }),
-  },
-});
+export default defineConfig(basePreset);
